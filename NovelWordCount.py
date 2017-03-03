a64b3f3388c10b284086fd80ff27dc5ad8dc8fb6
NyanCat12 <ldj.000000@gmail.com>
2017-03-03 18:04:49 +0800

    加入词向量

--- NovelWordCount.py
+++ NovelWordCount.py
@@ -11,10 +11,22 @@ def NovelRe(Novel):
     Count = Counter(words).most_common(100)
     return Count
 
-def main():
+def WordListToWordDict(WordList):
+    Dict = {}
+    for word in WordList:
+        Dict[word[0]] = word[1]
+    return Dict
+
+def FullWordList(OldList, AddDict):
+    for key in AddDict:
+        OldList.append(key)
+    NewList = list(set(OldList))
+    return NewList
+
+def Output():
     NovelList = ['a tale of two cities(双城记).txt', 'Aesop’s Fables(伊索寓言).txt', 'Jane Eyre(简·爱).txt', 'Oliver Twist(雾都孤儿(孤星血泪)).txt', 'Romeo and Juliet(罗蜜欧和朱丽叶).txt']
     for novel in NovelList:
-        print (novel[:-4]+'\n'+'========================================')
+        print ('Novel: '+novel+'\n'+'========================================')
         WordList = NovelRe(novel)
         i = 1
         for word in WordList:
@@ -22,6 +34,39 @@ def main():
             i += 1
         print ('\n')
 
+def Freq(Dict):
+    WordSum = 0
+    for key in Dict:
+        WordSum += Dict[key]
+    for key in Dict:
+        Dict[key] = Dict[key]/WordSum
+    return Dict
+
+def Style():
+    NovelList = ['a tale of two cities(双城记).txt', 'Aesop’s Fables(伊索寓言).txt', 'Jane Eyre(简·爱).txt', 'Oliver Twist(雾都孤儿(孤星血泪)).txt', 'Romeo and Juliet(罗蜜欧和朱丽叶).txt']
+    Dict = {}
+    for novel in NovelList:
+        Dict[novel[:-4]] = Freq(WordListToWordDict(NovelRe(novel)))
+    WordList = []
+    for key in Dict:
+        WordList = FullWordList(WordList, Dict[key])
+    return WordList, Dict
+        
+def WordVector(WordList, Dict):
+    vector = [x*0 for x in range(len(WordList))]
+    for index in range(len(WordList)):
+        try:
+            vector[index] = Dict[WordList[index]]
+        except:
+            vector[index] = 0
+    return vector
+
+def NovelVector():
+    WordList, Dict = Style()
+    NovelVectorDict = {}
+    for key in Dict:
+        NovelVectorDict[key] = WordVector(WordList, Dict[key])
+    return NovelVectorDict
+
 if __name__ == '__main__':
-    main()
-    
+    print (NovelVector())
