e7e97e20b7e8b81e0e2bbeca3d15f13cef019fd2
NyanCat12 <ldj.000000@gmail.com>
2017-04-06 15:39:38 +0800

    Update Prisoner.py

--- 20170331/Prisoner.py
+++ 20170331/Prisoner.py
@@ -1,5 +1,7 @@
-#Crossin 2017/03/31
-def Start_behavior(strategy):
+# Crossin 2017/03/31
+
+
+def Start_behavior(strategy):  # 根据不同策略确定第一轮的行为，0代表否认，1代表揭发
     if strategy == 'nice':
         return 0
     elif strategy == 'rat':
@@ -7,9 +9,10 @@ def Start_behavior(strategy):
     elif strategy == 'tit_for_tat':
         return 0
     elif strategy == 'my_strategy':
-        return 0
+        return 0  # 这里自定义自己的策略
 
-def Update_behavior(strategy, behavior1, behavior2):
+
+def Update_behavior(strategy, behavior1, behavior2):  # 根据自己的策略、上一轮双方的行为，确定下一轮的行为
     if strategy == 'nice':
         return 0
     elif strategy == 'rat':
@@ -17,23 +20,27 @@ def Update_behavior(strategy, behavior1, behavior2):
     elif strategy == 'tit_for_tat':
         return behavior2
     elif strategy == 'my_strategy':
-        return 0
+        return 0  # 这里自定义自己的策略
 
-def Step_result(behavior1, behavior2):
+
+def Step_result(behavior1, behavior2):  # 对于双方行为的四种组合，用一个式子算出双方要关几年
     return [-1*behavior1+4*behavior2-2*behavior1*behavior2+1, 4*behavior1-1*behavior2-2*behavior1*behavior2+1]
 
-def prisoner_dilemma(N, strategy1, strategy2):
-    result = [0, 0]
-    behavior = [Start_behavior(strategy1),Start_behavior(strategy2)]
+
+def prisoner_dilemma(N, strategy1, strategy2):  # N：轮数； Strategy1：主角策略； Strategy2：另一个人的策略
+    result = [0, 0]  # ；两个人判几年
+    behavior = [Start_behavior(strategy1),Start_behavior(strategy2)]  # 第一轮选择
     for step in range(N):
-        result[0] += Step_result(behavior[0], behavior[1])[0]
-        result[1] += Step_result(behavior[0], behavior[1])[1]
+        result[0] += Step_result(behavior[0], behavior[1])[0]  # 每一轮选择会导致主角判几年
+        result[1] += Step_result(behavior[0], behavior[1])[1]  # 每一轮选择会导致另一个人判几年
         behavior = [Update_behavior(strategy1, behavior[0], behavior[1]), Update_behavior(strategy2, behavior[1], behavior[0])]
+        # 每一轮行为会导致双方下一轮做出什么选择
     return (result[0], result[1])
 
+
 if __name__ == '__main__':
-    print (prisoner_dilemma(4,'nice', 'nice'))
-    print (prisoner_dilemma(5,'rat', 'rat'))
-    print (prisoner_dilemma(6,'nice', 'rat'))
-    print (prisoner_dilemma(4,'rat', 'tit_for_tat'))
-    print (prisoner_dilemma(7,'tit_for_tat', 'tit_for_tat'))
+    print(prisoner_dilemma(4, 'nice', 'nice'))
+    print(prisoner_dilemma(5, 'rat', 'rat'))
+    print(prisoner_dilemma(6, 'nice', 'rat'))
+    print(prisoner_dilemma(4, 'rat', 'tit_for_tat'))
+    print(prisoner_dilemma(7, 'tit_for_tat', 'tit_for_tat'))
