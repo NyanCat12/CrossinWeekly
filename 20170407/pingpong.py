aea9ec76290bd13e2f72174c13fce403c2e6e340
NyanCat12 <ldj.000000@gmail.com>
2017-04-07 16:10:14 +0800

    Update pingpong.py

--- 20170407/pingpong.py
+++ 20170407/pingpong.py
@@ -1,22 +1,20 @@
 # Crossin 2017/04/07
 
 
-def pingpong(n, label = 1):
+def pingpong(n, k=7, label=1):
     pingpong_list = [1]
     try:
         for i in range(1, n+1):
-            label *= (i % 7 == 0 or '7' in str(i))*-2+1
+            label *= (i % k == 0 or str(k) in str(i))*-2+1
             pingpong_list.append(pingpong_list[-1] + label)
-        print( pingpong_list[-2])
+        print(pingpong_list[-2])
     except:
         print('input error')
     return 0
 
 
 if __name__ == '__main__':
-    pingpong(7)
-    pingpong(8)
-    pingpong(21)
-    pingpong(100)
-    pingpong(-1)
-    pingpong(1.0)
+    pingpong(7, 7)
+    pingpong(8, 8)
+    pingpong(55, 6)
+    pingpong(100, 9)
