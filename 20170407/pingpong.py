7efe0af8e4fe0e27e1ee874ed3d7879d054185cb
NyanCat12 <ldj.000000@gmail.com>
2017-04-13 09:16:10 +0800

    Update pingpong.py

--- 20170407/pingpong.py
+++ 20170407/pingpong.py
@@ -4,7 +4,7 @@
 def pingpong(n, k=7, step=1):
     pingpong_list = [1]
     try:
-        for i in range(1, n+1):  # 和下面的打印数组倒数第二个数一起，可以处理负数和0的输入
+        for i in range(1, n+1):  # 和下面的打印列表倒数第二个数一起，可以处理负数和0的输入
             step *= (i % k == 0 or str(k) in str(i))*-2+1
             pingpong_list.append(pingpong_list[-1] + step)
         print(pingpong_list[-2])
